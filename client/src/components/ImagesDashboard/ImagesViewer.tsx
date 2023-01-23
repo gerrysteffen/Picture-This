3fba47cad230b7bfae022f9baa6de98e46a224ca
PBirley <pbirley101@gmail.com>
2023-01-23 15:35:41 +0000

    varying columns

--- client/src/components/ImagesDashboard/ImagesViewer.tsx
+++ client/src/components/ImagesDashboard/ImagesViewer.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { ImageList } from '@mui/material'
 import { AlbumType } from '../../types'
 import ImageView from './ImageView';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/system';
+import { Masonry } from '@mui/lab';
+
 
 type ImagesViewerType = {
   album: AlbumType, 
   setAlbum(album:AlbumType):void, 
   userId:string
 }
-
 
 export default function ImagesViewer({album, setAlbum, userId}:ImagesViewerType) {
   const deleteImage = (index:number) => {
@@ -17,12 +20,25 @@ export default function ImagesViewer({album, setAlbum, userId}:ImagesViewerType)
     newAlbum.photos.splice(index,1)
     setAlbum(newAlbum);
   }
+
+  
   return (
-    <ImageList variant="masonry" cols={3} gap={8}>
-    {album.photos.map((item, index) => (
+    <Masonry
+      columns={{ xs: 1, sm: 2, md: 4 }}
+      spacing={2}
+      defaultHeight={450}
+      defaultColumns={4}
+      defaultSpacing={1}
+    >
+      {album.photos.map((item, index) => (
       <ImageView key={item._id} item={item} index={index} deleteImage={deleteImage} userId={userId} />
-    ))}
-    </ImageList>
+      ))}
+    </Masonry>
+    // <ImageList variant="masonry" cols={3} gap={8}>
+    //   {album.photos.map((item, index) => (
+    //     <ImageView key={item._id} item={item} index={index} deleteImage={deleteImage} userId={userId} />
+    //   ))}
+    // </ImageList>
   )
 }
 
